b8a41b0b924190c2ab410b92c20b53576fbc2ec8
AFHerkDriver <usafsentinel@gmail.com>
2026-06-24 22:23:22 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Flight Card service worker */
-var CACHE = 'flightcard-v8';
+var CACHE = 'flightcard-v10';
 var CORE = ['./', './index.html', './manifest.json'];
 var ICONS = ['./icon-192.png', './icon-512.png', './icon-512-maskable.png', './apple-touch-icon.png', './favicon.png'];
 
